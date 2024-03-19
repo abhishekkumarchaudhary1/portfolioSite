638352b1dd1de0302040f10ffac3f9479f519456
abhishekkumarchaudhary1 <abhishek.chaudhary33@gmail.com>
2024-03-19 15:35:47 +0530

    Completing the design part for the works(projects) section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/about.css'
 import '../styles/components/featured.css'
+import '../styles/components/work.css'
 
 
 import '../styles/utils.css'
